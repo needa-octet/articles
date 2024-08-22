7c8115668e6e852d1d59f8c6ad8a4dedce8ac4a6
needa-octet <needa@octet.com>
2024-08-22 17:56:20 +0530

    first commit

--- d1/settings.py
+++ d1/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-mfl%ws+ck^&med-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG'))=="1"
 
-
+ALLOWED_HOSTS=[]
 if not DEBUG:
     ALLOWED_HOSTS+=[os.environ.get('DJANGO_ALLOWED_HOST')]
 else:
