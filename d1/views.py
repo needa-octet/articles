1b715952e15eb44b1147b0b8497bf38c56e584b7
needa-octet <needa@octet.com>
2024-08-22 17:24:13 +0530

    first commit

--- d1/views.py
+++ d1/views.py
@@ -8,18 +8,13 @@ from django.template.loader import render_to_string,get_template
 from django.shortcuts import get_object_or_404
 name ="hy"
 num=random.randint(1,4)
-article_obj=get_object_or_404(Article, id=2)
-article_title=article_obj.title
+
 article_list=Article.objects.all()
 my_list=[1,3,45,4536,1]
 my_list_str=""
 for x in my_list:
     my_list_str+=f"<li>number is: {x}\n</li>"
-context={
-    "title":article_obj.title,
-    "content":article_obj.content,
-    "my_list_str":article_list
-}
+
 # HTML_STRING= """
 
 # <h1>Hello {title} - {content}</h1>
@@ -29,5 +24,12 @@ context={
 HTML_STRING=render_to_string("home-view.html",context=context)
 
 def home_view(_request,id=None,*args, **kwargs):
+    article_obj=get_object_or_404(Article, id=2)
+    # article_title=article_obj.title
+    context={
+    "title":article_obj.title,
+    "content":article_obj.content,
+    "my_list_str":article_list
+}
     # print("id of Article",id)
     return HttpResponse(HTML_STRING)
